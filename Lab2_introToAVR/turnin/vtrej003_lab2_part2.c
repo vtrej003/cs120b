15a528223237bdb77e37d0a0feac7d02f1e0f1d7
Vincent <vtrej003@ucr.edu>
2020-01-17 06:57:26 -0800

    Completed part 2

---
+++ Lab2_introToAVR/turnin/vtrej003_lab2_part2.c
@@ -0,0 +1,42 @@
+/*	Author: root
+ *  Partner(s) Name: henlo johnson
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+
+int main(void)
+{
+    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+    DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+    DDRD = 0xFF; PORTD = 0x00;
+    unsigned char tmpA = 0x00;
+    unsigned char pAvail = 0x04;
+    
+    while(1) 
+    {
+        PORTC = 0x04;
+	PORTD = 0x00;
+        tmpA = PINA;
+        
+	for(int i = 0 ; i < 4; i++)//lowest nib
+	{		    
+   	    if (tmpA & 0x01)
+	    {
+                PORTC--;                           
+            }
+	    else{ }
+	    tmpA = tmpA >> 1;
+	}
+    }
+    return 1;    
+}
+
